19a3c406012c0f9e9d2b65fcac5356ad7dda831c
anthony <quereantho@gmail.com>
2022-02-26 11:35:40 +0100

    :wheelchair: Better button color contrasts

--- components/shared/actions/Button/index.tsx
+++ components/shared/actions/Button/index.tsx
@@ -17,13 +17,14 @@ type VariantStylesType = {
 };
 
 const variantStyles: VariantStylesType = {
-  PRIMARY: 'py-2 px-4 text-white bg-primary-400 hover:bg-primary focus:outline-none focus:border-primary-700 focus:ring-primary active:bg-primary',
-  SECONDARY: 'py-1 px-3 text-text-default border-4 border-secondary transition-colors hover:bg-secondary hover:text-white focus:outline-none focus:border-secondary-700 focus:ring-secondary active:bg-secondary-700',
-  TERTIARY: 'py-2 px-4 text-white bg-tertiary-400 hover:bg-tertiary focus:outline-none focus:border-tertiary-700 focus:ring-tertiary active:bg-tertiary',
-  WARNING: 'py-2 px-4 text-white bg-warning-600 hover:bg-warning focus:outline-none focus:border-warning-700 focus:ring-warning active:bg-warning-700',
-  ERROR: 'py-2 px-4 text-white bg-error-600 hover:bg-error focus:outline-none focus:border-error-700 focus:ring-error active:bg-error-700',
-  SUCCESS: 'py-2 px-4 text-white bg-success-600 hover:bg-success focus:outline-none focus:border-success-700 focus:ring-success active:bg-success-700',
-  DISABLED: 'py-2 px-4 text-black bg-gray-400 cursor-default',
+  PRIMARY: 'text-white bg-primary-700 border-primary-700 hover:bg-primary hover:border-primary focus:outline-none focus:border-primary focus:ring-primary active:bg-primary',
+  SECONDARY: 'ext-text-default border-secondary transition-colors hover:bg-secondary hover:text-white focus:outline-none focus:border-secondary-700 focus:ring-secondary active:bg-secondary-700',
+  TERTIARY: 'text-white bg-tertiary-600 border-tertiary-600 hover:bg-tertiary hover:border-tertiary focus:outline-none focus:border-tertiary focus:ring-tertiary active:bg-tertiary',
+  DISABLED: 'text-black bg-gray-400 border-gray-400 cursor-default',
+
+  WARNING: 'text-white bg-warning-600 hover:bg-warning focus:outline-none focus:border-warning-700 focus:ring-warning active:bg-warning-700',
+  ERROR: 'text-white bg-error-600 hover:bg-error focus:outline-none focus:border-error-700 focus:ring-error active:bg-error-700',
+  SUCCESS: 'text-white bg-success-600 hover:bg-success focus:outline-none focus:border-success-700 focus:ring-success active:bg-success-700',
 }
 
 export function Button({
@@ -39,8 +40,9 @@ export function Button({
   const usedVariant = disabled ? 'DISABLED' : variant;
 
   return (
-    <button {...defaultProps} className={`rounded-lg ${variantStyles[usedVariant]} ${className}`} disabled={disabled}>
-      {icon && <FontAwesomeIcon icon={icon}  className={'mr-2'}/>}
+    <button {...defaultProps} className={`rounded-lg py-1 px-3 border-4 ${variantStyles[usedVariant]} ${className}`}
+            disabled={disabled}>
+      {icon && <FontAwesomeIcon icon={icon} className={'mr-2'}/>}
       {children}
     </button>
   )
