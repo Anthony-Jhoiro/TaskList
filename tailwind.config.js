4d6e62f1fed5958a8a66195fcfb7e66d80fa6b05
anthony <quereantho@gmail.com>
2022-04-09 15:55:39 +0200

    :lipstick: Fix tailwind purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 module.exports = {
     purge: [
         './pages/**/*.{js,ts,jsx,tsx}',
-        './components/**/*.{js,ts,jsx,tsx}'
+        './components/**/*.{js,ts,jsx,tsx}',
+        './.storybook/**/*.{js,ts,jsx,tsx}',
     ],
     darkMode: false, // or 'media' or 'class'
     theme: {
